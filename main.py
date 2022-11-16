757a0fb710908afc871f8654e6153eaccfed0108
wirthual <wirthra@gmail.com>
2022-11-15 20:17:23 -0800

    try to enable cors for bundesapi

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from fastapi_csv import FastAPI_CSV
+from fastapi.middleware.cors import CORSMiddleware
 
 CSV_URL = "https://webrigoletto.uba.de/Rigoletto/Home/GetClassificationFile/Export_Tabelle"
 
+origins = [
+    "*"
+]
+
+
 app = FastAPI_CSV(CSV_URL,delimiter='|')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_headers=["*"],
+)
